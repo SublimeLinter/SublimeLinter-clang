fdb1a5d4df4da99c70f6de6d31999acee9a6ce0d
herr kaste <herr.kaste@gmail.com>
2018-04-26 12:45:20 +0200

    Showcase: Register two linters

--- linter.py
+++ linter.py
@@ -10,46 +10,43 @@
 
 """This module exports the Clang plugin class."""
 
-from SublimeLinter.lint import Linter, util
+import re
+from SublimeLinter.lint import Linter
 
 
-class Clang(Linter):
-    """Provides an interface to clang."""
+OUTPUT_RE = re.compile(
+    r'<stdin>:(?P<line>\d+):'
+    r'((?P<col>\d*): )?'  # column number, colon and space are only applicable for single line messages
+    # several lines of anything followed by
+    # either error/warning/note or newline (= irrelevant backtrace content)
+    # (lazy quantifiers so we don’t skip what we seek)
+    r'(.*?((?P<error>error)|(?P<warning>warning|note)|\r?\n))+?'
+    r': (?P<message>.+)',  # match the remaining content of the current line for output
+    re.MULTILINE
+)
 
-    regex = (
-        r'<stdin>:(?P<line>\d+):'
-        r'((?P<col>\d*): )?'  # column number, colon and space are only applicable for single line messages
-        # several lines of anything followed by
-        # either error/warning/note or newline (= irrelevant backtrace content)
-        # (lazy quantifiers so we don’t skip what we seek)
-        r'(.*?((?P<error>error)|(?P<warning>warning|note)|\r?\n))+?'
-        r': (?P<message>.+)'  # match the remaining content of the current line for output
-    )
 
-    multiline = True
-
+class clangc(Linter):
+    cmd = 'clang -fsyntax-only -fno-caret-diagnostics ${args} -'
     defaults = {
-        'selector': 'source.c, source.c++',
+        'selector': 'source.c',
         '-Wall': True,
         '-I +': [],
-        '-x': None
+        '-x': 'c'
     }
-
+    regex = OUTPUT_RE
+    multiline = True
     on_stderr = None
 
-    def cmd(self):
-        """Return the command line to execute."""
 
-        cmd = 'clang -fsyntax-only -fno-caret-diagnostics ${args} -'
-
-        settings = self.get_view_settings()
-
-        # If not set by the user apply magic
-        if settings['x'] is None:
-            syntax = util.get_syntax(self.view)
-            if syntax in ['c', 'c improved']:
-                settings['x'] = 'c'
-            elif syntax in ['c++', 'c++11']:
-                settings['x'] = 'c++'
-
-        return cmd
+class clangcplus(Linter):
+    cmd = 'clang -fsyntax-only -fno-caret-diagnostics ${args} -'
+    defaults = {
+        'selector': 'source.c++',
+        '-Wall': True,
+        '-I +': [],
+        '-x': 'c++'
+    }
+    regex = OUTPUT_RE
+    multiline = True
+    on_stderr = None
